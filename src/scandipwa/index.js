73084db112493496def5997ceb97ba7fbb22541b
yeegor <yegor.batov@gmail.com>
2020-06-15 12:45:05 +0300

    Add scandipwa/index

---
+++ src/scandipwa/index.js
@@ -0,0 +1,9 @@
+module.exports = [
+    "./app/plugin/Checkout.plugin.js",
+    "./app/plugin/CheckoutBilling.plugin.js",
+    "./app/plugin/CheckoutBillingContainer.plugin.js",
+    "./app/plugin/CheckoutContainer.plugin.js",
+    "./app/plugin/CheckoutPayments.plugin.js",
+    "./app/plugin/CheckoutPaymentsContainer.plugin.js",
+    "./sw/plugin/UrlHandler.plugin.js"
+];
